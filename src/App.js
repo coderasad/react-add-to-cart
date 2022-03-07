be1d69649943368b8d300b7bf3f0f47335574aaa
coderasad <asadidb43@gmail.com>
2022-03-07 20:46:19 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import {Navigate, Route, Routes} from "react-router-dom"
-import Main                      from "./component/Main";
-import Checkout                  from "./component/CheckOut";
-import NavBar                    from "./component/NavBar";
-import React, {useState}         from "react";
-import Cart                      from "./component/Cart";
-import Product                   from "./component/Product";
+import {Navigate, Route, Routes, useLocation} from "react-router-dom"
+import Main from "./component/Main";
+import Checkout from "./component/CheckOut";
+import NavBar from "./component/NavBar";
+import React, {useEffect, useState} from "react";
+import Cart from "./component/Cart";
+import Product from "./component/Product";
 
 function App() {
-    const [cartShow, setCartShow]       = useState(false)
-    const [cartProduct, setCartProduct] = useState([]);
+   const [cartShow, setCartShow]       = useState(false)
+   const [cartProduct, setCartProduct] = useState([]);
+   let location                        = useLocation();
 
-    return (<div className="App">
-            <NavBar cartShow={cartShow} setCartShow={setCartShow} cartProduct={cartProduct}/>
+   useEffect(() => {
+      setCartShow(false)
+   }, [location.pathname])
 
-            <Routes>
-                <Route path='/' element={<Product cartProduct={cartProduct} setCartProduct={setCartProduct} setCartShow={setCartShow} cartShow={cartShow} />}/>
-                <Route path='/checkout' element={<Checkout/>}/>
-            </Routes>
 
-            <Cart cartShow={cartShow} setCartShow={setCartShow} cartProduct={cartProduct}
-                  setCartProduct={setCartProduct}/>
-        </div>);
+   return (<div className="App">
+      <NavBar cartShow={cartShow} setCartShow={setCartShow} cartProduct={cartProduct}/>
+
+      <Routes>
+         <Route path='/'
+                element={<Product cartProduct={cartProduct} setCartProduct={setCartProduct} setCartShow={setCartShow}
+                                  cartShow={cartShow}/>}/>
+
+         <Route path='/checkout' element={<Checkout/>}/>
+      </Routes>
+
+      <Cart cartShow={cartShow} setCartShow={setCartShow} cartProduct={cartProduct}
+            setCartProduct={setCartProduct}/>
+   </div>);
 }
 
 export default App;
